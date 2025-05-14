3000bb133d4025e10b021873ed2725226deeb5f6
Saul Ulises Hernandez <saululises1912@gmail.com>
2025-05-13 21:26:33 -0600

    style: update footer styles to use flexbox layout
    config: allow Vite server access from other devices

--- src/routes/RoAbout.jsx
+++ src/routes/RoAbout.jsx
@@ -52,34 +52,67 @@ const CoSport = () => {
 	];
 
 	return (
-		<div className="tab-content">
-			<div
-				className="div-img"
-				style={{ backgroundImage: "url(" + imgSport + ")" }}
-			></div>
-			<div className="tab-content__info">
-				<div className="tab-content__info-text">
-					<h2>Me encanta nadar</h2>
-					<span>
-						{language == "ES"
-							? "Llevo nadando ya más de un año y no podría estar más enamorado de este deporte, es una sensación increíble el poder perderse en el agua después de un día de trabajo."
-							: "I have been swimming for more than a year now and I couldn't be more in love with this sport, it is an incredible feeling to be able to get lost in the water after a day of work."}
-						<br />
-						{language == "ES"
-							? "Si tuviera que calificar que hacer un tier-list de que estilo me gusta más..."
-							: "If I had to rate what style I like the most..."}
-					</span>
-					<div className="tab-content__info-text__list">
-						{styles.map((style, index) => (
-							<div
-								key={index}
-								className="tab-content__info-text__list-item"
-								style={{ opacity: 1 - index * 0.2 }}
-							>
-								<img src={style.icon} alt={style.text} />
-								<span>{style.text}</span>
-							</div>
-						))}
+		<div className="tab-body">
+			<div className="tab-content">
+				<div
+					className="div-img"
+					style={{ backgroundImage: "url(" + imgSport + ")" }}
+				></div>
+				<div className="tab-content__info">
+					<div className="tab-content__info-text">
+						<h2>Me encanta nadar</h2>
+						<span>
+							{language == "ES"
+								? "Llevo nadando ya más de un año y no podría estar más enamorado de este deporte, es una sensación increíble el poder perderse en el agua después de un día de trabajo."
+								: "I have been swimming for more than a year now and I couldn't be more in love with this sport, it is an incredible feeling to be able to get lost in the water after a day of work."}
+							<br />
+							{language == "ES"
+								? "Si tuviera que calificar que hacer un tier-list de que estilo me gusta más..."
+								: "If I had to rate what style I like the most..."}
+						</span>
+						<div className="tab-content__info-text__list">
+							{styles.map((style, index) => (
+								<div
+									key={index}
+									className="tab-content__info-text__list-item"
+									style={{ opacity: 1 - index * 0.2 }}
+								>
+									<img src={style.icon} alt={style.text} />
+									<span>{style.text}</span>
+								</div>
+							))}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+const CoMusic = () => {
+	const { language } = useContext(MyContext);
+
+	return (
+		<div className="tab-body">
+			<div className="tab-content">
+				<div
+					className="div-img"
+					style={{ backgroundImage: "url(" + imgSport + ")" }}
+				></div>
+				<div className="tab-content__info">
+					<div className="tab-content__info-text">
+						<h2>Me encanta nadar</h2>
+						<span>
+							{language == "ES"
+								? "Llevo nadando ya más de un año y no podría estar más enamorado de este deporte, es una sensación increíble el poder perderse en el agua después de un día de trabajo."
+								: "I have been swimming for more than a year now and I couldn't be more in love with this sport, it is an incredible feeling to be able to get lost in the water after a day of work."}
+							<br />
+							{language == "ES"
+								? "Si tuviera que calificar que hacer un tier-list de que estilo me gusta más..."
+								: "If I had to rate what style I like the most..."}
+						</span>
+						<div className="tab-content__info-text__list">
+						</div>
 					</div>
 				</div>
 			</div>
@@ -153,6 +186,10 @@ const RoAbout = () => {
 				</div>
 			</div>
 			{filtro === 1 && <CoSport />}
+			{filtro === 2 && <CoMusic />}
+			{filtro === 3 && <CoGames />}
+			{filtro === 4 && <CoManga />}
+			{filtro === 5 && <CoFood />}
 		</div>
 	);
 };
