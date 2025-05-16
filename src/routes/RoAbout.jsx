67798edc2aead0db43625185bde3e206fe6d2261
Saúl Ulises Hernández Cruz <saululises1912@gmail.com>
2025-05-16 08:33:04 -0600

    Enhance footer styles and add music list layout
    - Introduced a new footer body style with flex layout and padding.
    - Moved span styles to the new footer body section for better organization.
    - Added a music list section with responsive design, including music cards.
    - Implemented styles for music cards, including cover and info sections.
    - Ensured responsive adjustments for various screen sizes.

--- src/routes/RoAbout.jsx
+++ src/routes/RoAbout.jsx
@@ -16,6 +16,34 @@ import breast from "../assets/imgs/about/sport/breast.svg";
 import fly from "../assets/imgs/about/sport/fly.svg";
 import free from "../assets/imgs/about/sport/free.svg";
 
+import imgMusic from "../assets/imgs/about/music/foto.jpeg";
+import amp from "../assets/imgs/about/music/amp.svg";
+import electric from "../assets/imgs/about/music/electric.svg";
+import guitar from "../assets/imgs/about/music/guitar.svg";
+import melodic from "../assets/imgs/about/music/melodic.svg";
+import microphone from "../assets/imgs/about/music/microphone.svg";
+
+const API_KEY = "AIzaSyD786eN8Xt3Z-ItaSYVSDuZ4AVLrApAPD4";
+const PLAYLIST_ID = "PLC_vmjLKExTmybkcbAHXzqPsZuVH9Lc6I";
+
+const CoMusicCard = ({ cover, song, artist, album, year }) => {
+	return (
+		<div className="music-card">
+			<div
+				className="music-card__cover"
+				style={{ backgroundImage: `url(${cover})` }}
+			></div>
+			<div className="music-card__info">
+				<h3>{song}</h3>
+				<p>
+					{artist}{/*  - {album} */}
+				</p>
+				{/* <p id="year">{year}</p> */}
+			</div>
+		</div>
+	);
+};
+
 const CoTab = ({ status, icon, text }) => {
 	return (
 		<div className={`tab-buttons__item ${status}`}>
@@ -57,31 +85,149 @@ const CoSport = () => {
 				className="div-img"
 				style={{ backgroundImage: "url(" + imgSport + ")" }}
 			></div>
-			<div className="tab-content__info">
-				<div className="tab-content__info-text">
-					<h2>Me encanta nadar</h2>
-					<span>
-						{language == "ES"
-							? "Llevo nadando ya más de un año y no podría estar más enamorado de este deporte, es una sensación increíble el poder perderse en el agua después de un día de trabajo."
-							: "I have been swimming for more than a year now and I couldn't be more in love with this sport, it is an incredible feeling to be able to get lost in the water after a day of work."}
-						<br />
-						{language == "ES"
-							? "Si tuviera que calificar que hacer un tier-list de que estilo me gusta más..."
-							: "If I had to rate what style I like the most..."}
-					</span>
-					<div className="tab-content__info-text__list">
-						{styles.map((style, index) => (
-							<div
-								key={index}
-								className="tab-content__info-text__list-item"
-								style={{ opacity: 1 - index * 0.2 }}
-							>
-								<img src={style.icon} alt={style.text} />
-								<span>{style.text}</span>
-							</div>
-						))}
+			<div className="tab-content__body">
+				<div className="tab-content__info">
+					<div className="tab-content__info-text">
+						<h2>{language == "ES" ? "Me encanta nadar" : "I love swimming"}</h2>
+						<span>
+							{language == "ES"
+								? "Llevo nadando ya más de un año y no podría estar más enamorado de este deporte, es una sensación increíble el poder perderse en el agua después de un día de trabajo."
+								: "I have been swimming for more than a year now and I couldn't be more in love with this sport, it is an incredible feeling to be able to get lost in the water after a day of work."}
+							<br />
+							<br />
+							{language == "ES"
+								? "Si tuviera que calificar que hacer un tier-list de que estilo me gusta más..."
+								: "If I had to rate what style I like the most..."}
+						</span>
+						<div className="tab-content__info-text__list">
+							{styles.map((style, index) => (
+								<div
+									key={index}
+									className="tab-content__info-text__list-item"
+									style={{ opacity: 1 - index * 0.2 }}
+								>
+									<img src={style.icon} alt={style.text} />
+									<span>{style.text}</span>
+								</div>
+							))}
+						</div>
 					</div>
 				</div>
+			</div>
+		</div>
+	);
+};
+
+const CoMusic = () => {
+	const { language } = useContext(MyContext);
+
+	const instruments = [
+		{
+			icon: guitar,
+			text: language == "ES" ? "Guitarra" : "Guitar",
+		},
+		{
+			icon: electric,
+			text: language == "ES" ? "Guitarra eléctrica" : "Electric Guitar",
+		},
+		{
+			icon: melodic,
+			text: language == "ES" ? "Melódica" : "Melodica",
+		},
+		{
+			icon: microphone,
+			text: language == "ES" ? "Voz" : "Voice",
+		},
+		{
+			icon: amp,
+			text: language == "ES" ? "Amplificador" : "Amplifier",
+		},
+	];
+
+	const [songs, setSongs] = useState([]);
+
+	useEffect(() => {
+		const fetchSongs = async () => {
+			try {
+				const response = await fetch(
+					`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=10&playlistId=${PLAYLIST_ID}&key=${API_KEY}`
+				);
+				const data = await response.json();
+				console.table(data.items[0]);
+				
+
+				// Ordenar por fecha de publicación y tomar los últimos 3
+				const sortedSongs = data.items
+					.sort(
+						(a, b) =>
+							new Date(b.snippet.publishedAt) - new Date(a.snippet.publishedAt)
+					)
+					.slice(0, 3)
+					.map((item) => ({
+						title: item.snippet.title,
+						artist: item.snippet.videoOwnerChannelTitle.split(" - ")[0],
+						coverImage: item.snippet.thumbnails.maxres.url,
+						publishedAt: item.snippet.publishedAt,
+					}));
+
+				setSongs(sortedSongs);
+			} catch (error) {
+				console.error("Error fetching songs:", error);
+			}
+		};
+
+		fetchSongs();
+	}, []);
+
+	return (
+		<div className="tab-body">
+			<div className="tab-content">
+				<div
+					className="div-img"
+					style={{ backgroundImage: "url(" + imgMusic + ")" }}
+				></div>
+
+				<div className="tab-content__info">
+					<div className="tab-content__info-text">
+						<h2>
+							{language == "ES"
+								? "Escuchar y tocar música es maravilloso"
+								: "Listening to and playing music is wonderful"}
+						</h2>
+						<span>
+							{language == "ES"
+								? "Me encanta escuchar música y no solo como parte del fondo mientras comino por la ciudad, realmente me encanta darme mi tiempo para sentarme y escuchar un buen álbum de PinkFloyd, Gorillaz o Joji."
+								: "I love listening to music and not just as background music while walking around the city, I really enjoy taking my time to sit down and listen to a good album by Pink Floyd, Gorillaz, or Joji."}
+							<br />
+							<br />
+							{language == "ES"
+								? "También toco y canto; me gusta mucho tomar mi guitarra, mi melódica o la guitarra eléctrica, prender el amplificador y tocar por horas y horas."
+								: "I also play and sing; I really enjoy taking my guitar, my melodica, or the electric guitar, turning on the amplifier, and playing for hours and hours."}
+						</span>
+						<div className="tab-content__info-text__list">
+							{instruments.map((instrument, index) => (
+								<div key={index} className="tab-content__info-text__list-item">
+									<img src={instrument.icon} alt={instrument.text} />
+								</div>
+							))}
+						</div>
+					</div>
+				</div>
+			</div>
+			<span>
+				{language == "ES"
+					? "Ultimamente he estado escuchando:"
+					: "Recently, I have been listening to:"}
+			</span>
+			<div className="music-list">
+				{songs.map((song, index) => (
+					<CoMusicCard
+						key={index}
+						cover={song.coverImage}
+						song={song.title}
+						artist={song.artist}
+					/>
+				))}
 			</div>
 		</div>
 	);
@@ -90,7 +236,7 @@ const CoSport = () => {
 const RoAbout = () => {
 	const { language, setRuta } = useContext(MyContext);
 
-	const [filtro, setFiltro] = useState(1);
+	const [filtro, setFiltro] = useState(2);
 
 	useEffect(() => {
 		setRuta("/about-me");
@@ -153,6 +299,7 @@ const RoAbout = () => {
 				</div>
 			</div>
 			{filtro === 1 && <CoSport />}
+			{filtro === 2 && <CoMusic />}
 		</div>
 	);
 };
